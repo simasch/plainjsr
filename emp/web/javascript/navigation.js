a8e83f6df4e0f878795477e14b12394651b7f624
Simon Martinelli <sm@simas.ch>
2013-04-29 21:37:32 +0200

    Navigation

--- emp/web/javascript/navigation.js
+++ emp/web/javascript/navigation.js
@@ -1,3 +1,14 @@
 function includeNavigation() {
-    document.getElementById('navigation').innerHTML = '<a href="index.html">Home</a>';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/emp/navigation.html', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                document.getElementById('navigation').innerHTML = xhr.responseText;
+            } else {
+                alert(xhr.status);
+            }
+        }
+    };
+    xhr.send(null);
 }
