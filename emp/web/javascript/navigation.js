7b3fe323381d7d3f3c3daa885655555dde8082d9
Simon Martinelli <sm@simas.ch>
2013-05-01 15:58:45 +0200

    simplified code

--- emp/web/javascript/navigation.js
+++ emp/web/javascript/navigation.js
@@ -1,14 +1,5 @@
 function includeNavigation() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/emp/navigation.html', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                document.getElementById('navigation').innerHTML = xhr.responseText;
-            } else {
-                alert(xhr.status);
-            }
-        }
-    };
-    xhr.send(null);
+    sendRequest('GET', '/emp/navigation.html', 200, function(response) {
+        document.getElementById('navigation').innerHTML = response;
+    });
 }
